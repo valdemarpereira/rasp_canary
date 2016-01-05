95b50de18852ab15727c81371abf98ff85d297ef
Valdemar Pereira <valdemar.pereira@gmail.com>
2016-01-05 23:01:27 +0000

    adding the skeleton for the PanTilt Control

---
+++ pan_tilt_control.py
@@ -0,0 +1,19 @@
+from servo_control import ServoControl
+
+class PanTillControl:
+
+    def __init__(self, horzServo, vertServo):
+        self.horzServo = horzServo
+        self.vertServo = vertServo
+
+    def moveUp(self, angle):
+
+    def moveDown(self, angle):
+
+
+    def moveLeft(self, angle):
+
+
+    def moveRight(self, angle):
+
+
