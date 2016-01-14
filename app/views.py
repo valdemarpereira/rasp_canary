b74d6ee032639e6dfbf418f37c5ab868dc61ec36
Valdemar Pereira <valdemar.pereira@gmail.com>
2016-01-14 13:42:39 +0000

    fix imports

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from app import app
 from app.camera_pi import Camera
+from flask import render_template, Response
 
 @app.route('/')
 def index():
     """Video streaming home page."""
-    return app.render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/ping')
 def ping():
@@ -21,7 +22,7 @@ def gen(camera):
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return app.Response(gen(Camera()),
+    return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
