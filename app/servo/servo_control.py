eac04e2940dd9e986f400e890aec528db468bb96
Valdemar Pereira <valdemar.pereira@gmail.com>
2016-01-20 19:11:29 +0000

    .

--- app/servo/servo_control.py
+++ app/servo/servo_control.py
@@ -11,7 +11,7 @@ class ServoControl:
         GPIO.setup(pinNumber,GPIO.OUT)
         self.pwm=GPIO.PWM(pinNumber,50)
         self.pwm.start((self.maxDutyCycle - self.minDutyCycle) / 2)
-        print("Servo Init on " + self.pinNumber)
+        print("Servo Init on " + str(self.pinNumber))
 
     def rotateLeft(self, angle):
         self.pwm.ChangeDutyCycle(self.calculateDutyCycle(angle))
